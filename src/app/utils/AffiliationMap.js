0cde609f59f4a37f2397b192f3eee699c336c16f
Mal <zillyman@protonmail.com>
2019-07-18 19:11:44 +0000

    I did dumb stuff

--- src/app/utils/AffiliationMap.js
+++ src/app/utils/AffiliationMap.js
@@ -1,23 +1,8 @@
 const map = {
-    //steemit
-    ned: 'steemit',
-    justinw: 'steemit',
-    elipowell: 'steemit',
-    vandeberg: 'steemit',
-    birdinc: 'steemit',
-    gerbino: 'steemit',
-    andrarchy: 'steemit',
-    roadscape: 'steemit',
-    steemitblog: 'steemit',
-    steemitdev: 'steemit',
 
-    /*
-    //steem monsters
-    steemmonsters: 'sm',
-    'steem.monsters': 'sm',
-    aggroed: 'sm',
-    yabapmatt: 'sm',
-*/
+    // STEMGeeks
+    themarkymark: 'STEMGeeks Team',
+    stemgeeks: 'STEMGeeks Team'
 };
 
 export default map;
--- src/server/server-html.jsx
+++ src/server/server-html.jsx
@@ -60,105 +60,29 @@ export default function ServerHTML({
                 <link rel="manifest" href="/static/manifest.json" />
                 <link rel="icon" type="image/x-icon" href="/favicon.ico?v=2" />
                 <link
-                    rel="apple-touch-icon-precomposed"
-                    sizes="57x57"
-                    href="/images/favicons/apple-touch-icon-57x57.png"
-                    type="image/png"
-                />
-                <link
-                    rel="apple-touch-icon-precomposed"
-                    sizes="114x114"
-                    href="/images/favicons/apple-touch-icon-114x114.png"
-                    type="image/png"
-                />
-                <link
-                    rel="apple-touch-icon-precomposed"
-                    sizes="72x72"
-                    href="/images/favicons/apple-touch-icon-72x72.png"
-                    type="image/png"
-                />
-                <link
-                    rel="apple-touch-icon-precomposed"
-                    sizes="144x144"
-                    href="/images/favicons/apple-touch-icon-144x144.png"
-                    type="image/png"
-                />
-                <link
-                    rel="apple-touch-icon-precomposed"
-                    sizes="60x60"
-                    href="/images/favicons/apple-touch-icon-60x60.png"
-                    type="image/png"
-                />
-                <link
-                    rel="apple-touch-icon-precomposed"
-                    sizes="120x120"
-                    href="/images/favicons/apple-touch-icon-120x120.png"
-                    type="image/png"
-                />
-                <link
-                    rel="apple-touch-icon-precomposed"
-                    sizes="76x76"
-                    href="/images/favicons/apple-touch-icon-76x76.png"
-                    type="image/png"
-                />
-                <link
-                    rel="apple-touch-icon-precomposed"
-                    sizes="152x152"
-                    href="/images/favicons/apple-touch-icon-152x152.png"
-                    type="image/png"
-                />
-                <link
                     rel="icon"
                     type="image/png"
-                    href="/images/favicons/favicon-196x196.png"
-                    sizes="196x196"
-                />
-                <link
-                    rel="icon"
-                    type="image/png"
-                    href="/images/favicons/favicon-96x96.png"
-                    sizes="96x96"
-                />
-                <link
-                    rel="icon"
-                    type="image/png"
-                    href="/images/favicons/favicon-32x32.png"
-                    sizes="32x32"
-                />
-                <link
-                    rel="icon"
-                    type="image/png"
-                    href="/images/favicons/favicon-16x16.png"
                     sizes="16x16"
+                    href="/images/favicons/favicon-16x16.png"
                 />
                 <link
                     rel="icon"
                     type="image/png"
-                    href="/images/favicons/favicon-128.png"
-                    sizes="128x128"
+                    sizes="32x32"
+                    href="/images/favicons/favicon-32x32.png"
                 />
-                <meta name="application-name" content={APP_NAME} />
-                <meta name="msapplication-TileColor" content="#FFFFFF" />
+                <link
+                    rel="icon"
+                    type="image/png"
+                    sizes="96x96"
+                    href="/images/favicons/favicon-96x96.png"
+                />	    
+                <meta name="msapplication-TileColor" content="#ffffff" />
                 <meta
                     name="msapplication-TileImage"
-                    content="/images/favicons/mstile-144x144.png"
+                    content="/images/favicons/ms-icon-144x144.png"
                 />
-                <meta
-                    name="msapplication-square70x70logo"
-                    content="/images/favicons/mstile-70x70.png"
-                />
-                <meta
-                    name="msapplication-square150x150logo"
-                    content="/images/favicons/mstile-150x150.png"
-                />
-                <meta
-                    name="msapplication-wide310x150logo"
-                    content="/images/favicons/mstile-310x150.png"
-                />
-                <meta
-                    name="msapplication-square310x310logo"
-                    content="/images/favicons/mstile-310x310.png"
-                />
+                <meta name="theme-color" content="#ffffff" />
                 <link
                     href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600"
                     rel="stylesheet"
